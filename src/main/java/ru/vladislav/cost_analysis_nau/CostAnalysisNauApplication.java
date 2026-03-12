79072f10b2929d8e8f87709aafa0094f63364e05
Vladislav <vlad.utrobin.2006@mail.ru>
2026-03-12 12:34:54 +0500

    Init project

---
+++ src/main/java/ru/vladislav/cost_analysis_nau/CostAnalysisNauApplication.java
@@ -0,0 +1,13 @@
+package ru.vladislav.cost_analysis_nau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostAnalysisNauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostAnalysisNauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vladislav/cost_analysis_nau/CostAnalysisNauApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladislav.cost_analysis_nau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostAnalysisNauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
