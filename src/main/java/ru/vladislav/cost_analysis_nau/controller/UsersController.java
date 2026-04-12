ccd8d254974d400538a3ef11105eecbbbfc61bf4
Vladislav Utrobin <vlad.utrobin.2006@mail.ru>
2026-04-12 12:06:46 +0500

    resolve hw

---
+++ src/main/java/ru/vladislav/cost_analysis_nau/controller/UsersController.java
@@ -0,0 +1,19 @@
+package ru.vladislav.cost_analysis_nau.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.vladislav.cost_analysis_nau.repository.UsersRepository;
+
+@RequiredArgsConstructor
+@Controller
+public class UsersController {
+    private final UsersRepository usersRepository;
+
+    @GetMapping("/users/view/list")
+    public String getListUsers(Model model){
+        model.addAttribute("users", usersRepository.findAll());
+        return "users";
+    }
+}
--- src/main/java/ru/vladislav/cost_analysis_nau/repository/CategoryRepository.java
+++ src/main/java/ru/vladislav/cost_analysis_nau/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package ru.vladislav.cost_analysis_nau.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import ru.vladislav.cost_analysis_nau.entity.Category;
 
-@Repository
+@RepositoryRestResource(path = "category")
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Category findCategoryByName(@Param("name") String name);
+    boolean existsCategoryByName(@Param("name") String name);
 }
