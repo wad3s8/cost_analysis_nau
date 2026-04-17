111bd17a01e58b82752fef8d810dcaee663ba012
Vladislav Utrobin <vlad.utrobin.2006@mail.ru>
2026-04-17 23:15:46 +0500

    fix bugs

--- src/main/java/ru/vladislav/cost_analysis_nau/repository/UsersRepository.java
+++ src/main/java/ru/vladislav/cost_analysis_nau/repository/UsersRepository.java
@@ -3,6 +3,7 @@ package ru.vladislav.cost_analysis_nau.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import ru.vladislav.cost_analysis_nau.dto.CreateUser;
 import ru.vladislav.cost_analysis_nau.entity.UserApp;
 
 @Repository
